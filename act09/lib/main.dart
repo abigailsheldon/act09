6577dea13d06f2f488e14386209279b50adf9cb6
abigailsheldon <abigail.l.sheldon@gmail.com>
2025-03-05 19:09:14 -0500

    Adds basic skeleton code for screens

--- act09/lib/main.dart
+++ act09/lib/main.dart
@@ -40,37 +40,10 @@ class _FoldersScreenState extends State<FoldersScreen> {
 
 }
 
+class CardsScreen extends StatefulWidget {
 
+}
 
-  @override
-  Widget build(BuildContext context) {
-    
-    return Scaffold(
-      appBar: AppBar(
-        
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        
-        title: Text(widget.title),
-      ),
-      body: Center(
-        
-        child: Column(
-          
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), 
-    );
-  }
+class _CardsScreenState extends State<CardsScreen> {
+
 }
